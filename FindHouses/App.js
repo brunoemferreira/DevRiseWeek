785524c983256b264654bb017846a0283e95f6e9
brunoemferreira <brunoemf@gmail.com>
2021-03-24 20:34:09 -0300

    App.js Update

--- FindHouses/App.js
+++ FindHouses/App.js
@@ -9,32 +9,6 @@ import {
   View,
 } from 'react-native';
 
-const Section = ({ children, title }): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App = () => {
   return <SafeAreaView />;
 };
