81690d751be36b6faa77550ead7dfb0cbf7f666e
brunoemferreira <brunoemf@gmail.com>
2021-03-25 18:57:40 -0300

    Installing Storybook

--- FindHouses/src/App.js
+++ FindHouses/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { SplashScreen } from './screens';
 import { theme } from './styles/theme';
+import StoryBook from '../storybook';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <SplashScreen />
+      <StoryBook />
     </ThemeProvider>
   );
 };
