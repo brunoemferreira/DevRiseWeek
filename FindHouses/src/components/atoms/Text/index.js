2060fe1ed59cfe690c615842693d182d8e709e11
brunoemferreira <brunoemf@gmail.com>
2021-03-26 22:07:21 -0300

    Creating Input Component

--- FindHouses/src/components/atoms/Text/index.js
+++ FindHouses/src/components/atoms/Text/index.js
@@ -7,9 +7,9 @@ export const Title = styled.Text`
 `;
 
 export const InputLabel = styled.Text`
-  font-size: ${({ theme }) => theme.metrics.px(14)}px;
+  font-size: ${({ theme }) => theme.metrics.px(16)}px;
   color: white;
-  font-weight: 600;
+  font-weight: bold;
 `;
 
 export const ButtonText = styled.Text`
